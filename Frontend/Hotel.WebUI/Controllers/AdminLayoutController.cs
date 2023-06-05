954bea14f315317460f5b2dcf22a84698fcddd99
hcemmm <hcemmm@gmail.com>
2023-06-05 17:29:44 +0300

    UI düzeltildi.

---
+++ Frontend/Hotel.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.WebUI.Controllers
+{
+	public class AdminLayoutController : Controller
+	{
+		public IActionResult _AdminLayout()
+		{
+			return View();
+		}
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
