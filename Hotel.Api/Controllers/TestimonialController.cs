d92a7e31ba6f98972dbf6a3b9dab63626f7f3fc6
hcemmm <hcemmm@gmail.com>
2023-06-06 12:23:51 +0300

    Testimonial Eklendi

--- Hotel.Api/Controllers/TestimonialController.cs
+++ Hotel.Api/Controllers/TestimonialController.cs
@@ -29,7 +29,7 @@ namespace Hotel.Api.Controllers
 			_testimonialService.TInsert(testimonial);
 			return Ok();
 		}
-		[HttpDelete]
+		[HttpDelete("{id}")]
 		public IActionResult DeleteTestimonial(int id)
 		{
 			var values = _testimonialService.TGetById(id);
