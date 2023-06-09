ce5215668a451a748bea0a1ab832bfeefcc0252b
hcemmm <hcemmm@gmail.com>
2023-06-09 15:33:41 +0300

    Admin Oda update

--- Frontend/Hotel.WebUI/Controllers/RoomController.cs
+++ Frontend/Hotel.WebUI/Controllers/RoomController.cs
@@ -1,12 +1,94 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel.WebUI.Dtos.RoomDto;
+using Hotel.WebUI.Dtos.RoomDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Hotel.WebUI.Controllers
 {
     public class RoomController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RoomController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:17194/api/Room");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<RoomResultDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddRoom()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRoom(RoomCreateDto roomCreateDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(roomCreateDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:17194/api/Room", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Room");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteRoom(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:17194/api/Room/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateRoom(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:17194/api/Room/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<RoomUpdateDto>(jsonData);
+                return View(values);
+            }
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRoom(RoomUpdateDto roomUpdateDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(roomUpdateDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:17194/api/Room/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
---
+++ Frontend/Hotel.WebUI/Dtos/RoomDto/RoomCreateDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.WebUI.Dtos.RoomDto
+{
+    public class RoomCreateDto
+    {
+        [Required(ErrorMessage = "Oda numarası giriniz")]
+        public string RoomNumber { get; set; }
+
+        [Required(ErrorMessage = "Oda resmi giriniz")]
+        public string RoomCoverImage { get; set; }
+
+        [Required(ErrorMessage = "Oda fiyatı giriniz")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Oda başlığı giriniz")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Oda yatak sayısı giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı en fazla 100 karakter olabilir")]
+        public string BedCount { get; set; }
+
+        [Required(ErrorMessage = "Oda banyo sayısı giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı en fazla 100 karakter olabilir")]
+        public string BathCount { get; set; }
+
+        [Required(ErrorMessage = "Oda wifi durumu giriniz")]
+        public bool Wifi { get; set; }
+
+        [Required(ErrorMessage = "Oda açıklaması giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı en fazla 100 karakter olabilir")]
+        public string Description { get; set; }
+    }
+}
